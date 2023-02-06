b72e3fdb4f1c6c45413cfbdc344fa51211bad3d6
newli <newri0807@gmail.com>
2023-02-06 17:57:50 +0900

    수정2

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import styles from "./Nav.module.css";
 import { BsFillSunFill } from "react-icons/bs";
 import { MdNightlightRound } from "react-icons/md";
